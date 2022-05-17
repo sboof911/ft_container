0c7d21d0e068a59217359f6032e4d2dc87eac48b
sboof911 <amaach@student.1337.ma>
2022-05-18 00:34:14 +0100

    all good

--- push.sh
+++ push.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 #to run the scipt :   sh push.sh Path TimeToSleepInMinits
+declare -i Time= $2 * 60
 while true
 do
     cp $1/* .
     git add *
     git commit -m "all good"
     git push
-    sleep $2m
+
+    sleep Time
 done
