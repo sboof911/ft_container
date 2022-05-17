ace04087e4cb5007d06c913395e3779d04397d8b
sboof911 <amaach@student.1337.ma>
2022-05-18 00:18:59 +0100

    all good

--- push.sh
+++ push.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-cp ../ft_container/* .
-git add *
-git commit -m "all good"
-git push
+while true
+do
+    cp ../ft_container/* .
+    git add *
+    git commit -m "all good"
+    git push
+    sleep 10m
+done
