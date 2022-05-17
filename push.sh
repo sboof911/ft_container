c5e9849318d031652fffaf18da3832b9aaf9c961
sboof911 <amaach@student.1337.ma>
2022-05-18 00:37:34 +0100

    all good

--- push.sh
+++ push.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #to run the scipt :   sh push.sh Path TimeToSleepInMinits
-declare -i Time= $2
-Time = Time * 60
-echo Time
+declare -i Time=$2
+Time=Time*60
+echo $Time
 while true
 do
     cp $1/* .
