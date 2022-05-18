8dc598c88651a8ac0d2c34d2a6b698baf612421d
sboof911 <amaach@student.1337.ma>
2022-05-18 20:56:10 +0100

    comment

--- myscript.sh
+++ myscript.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-#to run the scipt :   sh push.sh Path TimeToSleepInMinits
+#to run the scipt :   sh push.sh Path TimeToSleepInMinits 
 declare -i Time=$2
+if $3
+then
+    comment=$3
+else
+    comment="all good"
+fi
 Time=Time*60
 while true
 do
     cp $1/* .
     git add *
-    git commit -m "all good"
+    git commit -m comment
     git push
 
     sleep $Time
